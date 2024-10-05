f3b59407a01bd0653ab024fe41bf332cf0f0510c
hakim671 <boyzodahakim2008@gmail.com>
2024-10-05 19:10:46 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,25 @@ with st.expander("data-viz"):
   st.scatter_chart(data=df,x="bill_depth_mm",y='body_mass_g', color = 'species')
   st.scatter_chart( data= df, x = 'bill_depth_mm', y = 'sex', color = 'species')
   st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
+
+with st.sidebar:
+  st.header('Input features')
+  island = st.selectbox( 'Island', ( 'Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9 )
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2 )
+  flipper_length_mm = st.slider('Flipper depth (mm)', 172.8, 231.0, 201.0 )
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0 )
+  gender = st.selectbox('Gender', ('Male','Female'))
+  
+  
+  
+
+# Create a DataFrame for the input features
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
