d933637967dfd8778c13ea5ae68ef434cacd6481
hakim671 <boyzodahakim2008@gmail.com>
2026-06-10 12:36:39 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -148,7 +148,16 @@ if st.button("Предсказать"):
         model=model
     )
 
-    total = round(a['home_goals_pred']) + round(a['away_goals_pred'])
+ def round_to_half(x):
+    whole = int(x)
+    decimal = x - whole
+    
+    if decimal < 0.5:
+        return whole
+    else:
+        return whole + 0.5
+
+total = round_to_half(a['home_goals_pred']) + round_to_half(a['away_goals_pred'])
 
     st.write(f"{team1}: {a['home_goals_pred']:.2f}")
     st.write(f"{team2}: {a['away_goals_pred']:.2f}")
